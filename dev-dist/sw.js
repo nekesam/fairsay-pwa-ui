bf0de01927de03cfd599a492589aece4539b4230
Martha Nekesa <marthaanekesa@outlook.com>
2026-03-27 15:33:35 +0300

    feat: Added a call-to-action button on the Landing Page hero section that redirects users to the Whistleblowing page

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.m9s4bb3t2l8"
+    "revision": "0.6gn7u50be08"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
